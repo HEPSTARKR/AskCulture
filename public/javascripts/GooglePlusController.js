c3af08b77f6304ca1a62c51faae19ed45c1bd438
Astoration <ybdh00@naver.com>
2016-02-16 22:47:27 +0900

    LoginDialog Commit

---
+++ public/javascripts/GooglePlusController.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Sunrin on 2016. 2. 15..
+ */
+function onSuccess(googleUser) {
+    console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+}
+function onFailure(error) {
+    console.log(error);
+}
+function renderButton() {
+    gapi.signin2.render('googlePlusLoginButton', {
+        'scope': 'https://www.googleapis.com/auth/plus.login',
+        'width': 384,
+        'height': 40,
+        'longtitle': true,
+        'theme': 'dark',
+        'onsuccess': onSuccess,
+        'onfailure': onFailure
+    });
+}
---
+++ public/javascripts/userController.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Sunrin on 2016. 2. 16..
+ */
+$('.loginDialogCloseButton').click(function(){
+   $('#loginDialog-wrapper').hide();
+});
